86535b797cb1692caf10083313c319f631647b22
Stephan Wagner <stephanwagner.me@gmail.com>
2022-12-28 20:15:38 -0300

    Add textfield icons

--- htdocs/resources/src/js/backend/form/select.js
+++ htdocs/resources/src/js/backend/form/select.js
@@ -1,22 +1,21 @@
 import $ from 'jquery';
 import select2 from 'select2';
 
+// Init select2
 select2($);
 
+// Init select fields
 function initSelectFields() {
   $('[data-select-field]').each(function (index, item) {
     if (!$(item).data('select-field-initialized')) {
       $(item)
         .select2({
-          // minimumResultsForSearch: -1,
+          minimumResultsForSearch: -1,
           width: '100%',
           closeOnSelect: true
         })
-        .on('select2:open', function (e) {
-          $('input.select2-search__field').attr(
-            'placeholder',
-            'Search...'
-          );
+        .on('select2:open', function (ev) {
+          $('input.select2-search__field').attr('placeholder', 'Search...');
         });
       $(item).data('select-field-initialized', true);
     }
@@ -24,6 +23,8 @@ function initSelectFields() {
 }
 window.initSelectFields = initSelectFields;
 
+// Domready
 $(function () {
+  // Init select fields
   initSelectFields();
 });
---
+++ htdocs/resources/src/js/backend/form/textfield.js
@@ -0,0 +1,28 @@
+import $ from 'jquery';
+
+// Init password fields
+function initPasswordFields() {
+  $('[data-show-password-trigger]').each(function (index, item) {
+    if (!$(item).data('show-password-initialized')) {
+      $(item).on('click', function (ev) {
+        const wrapper = $(item).parents('[data-show-password-container]');
+        if (wrapper.length) {
+          const input = wrapper.find('[data-show-password-input]');
+          if (input.length) {
+            wrapper.toggleClass('-show-password');
+            const passwordIsShown = wrapper.hasClass('-show-password');
+            input.attr('type', passwordIsShown ? 'text' : 'password');
+          }
+        }
+      });
+      $(item).data('show-password-initialized', true);
+    }
+  });
+}
+window.initPasswordFields = initPasswordFields;
+
+// Domready
+$(function () {
+  // Init password fields
+  initPasswordFields();
+});
