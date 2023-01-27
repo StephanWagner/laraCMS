cfb9847f0f1e3f08e6708da3433229b13ab1ccac
Stephan Wagner <stephanwagner.me@gmail.com>
2023-01-27 14:38:04 +0000

    Update panel links

---
+++ htdocs/lang/de/backend/panel.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    // Labels
+    'label-content' => 'Inhalt',
+    'label-admin' => 'Admin',
+    'label-developer' => 'Entwickler',
+
+    // Links
+    'link-pages' => 'Seiten',
+    'link-media' => 'Media',
+    'link-settings' => 'Einstellungen',
+    'link-users' => 'Benutzer',
+    'link-menus' => 'Menüs',
+    'link-variables' => 'Variablen',
+    'link-post-types' => 'Inhaltslisten',
+    'link-blocks' => 'Blocks',
+];
---
+++ htdocs/lang/en/backend/panel.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    // Labels
+    'label-content' => 'Content',
+    'label-admin' => 'Admin',
+    'label-developer' => 'Developer',
+
+    // Links
+    'link-pages' => 'Pages',
+    'link-media' => 'Media',
+    'link-settings' => 'Settings',
+    'link-users' => 'Users',
+    'link-menus' => 'Menus',
+    'link-variables' => 'Variables',
+    'link-post-types' => 'Post types',
+    'link-blocks' => 'Blocks',
+];
