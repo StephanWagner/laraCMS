800fbd0a2efef238b3e385d068c6670e1607e536
Stephan Wagner <stephanwagner.me@gmail.com>
2025-08-30 22:29:01 +0700

    Add image placeholders

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -8,31 +8,64 @@ use App\Helpers\MediaHelper;
 
 class MediaController extends Controller
 {
+    private static $imageMimeTypes = [
+        'image/jpeg',
+        'image/png',
+        'image/webp',
+        'image/svg',
+        'image/svg+xml',
+        'image/gif',
+        'image/avif',
+        'image/x-icon',
+        'image/ico',
+        'image/vnd.microsoft.icon',
+    ];
+
     /**
      * Show original media file
      */
     public function show(?string $prefix, string $uuid, ?string $size = null)
     {
-        $media = Media::where('uuid', $uuid)->firstOrFail();
+        $media = Media::where('uuid', $uuid)->first();
+
+        if (!$media) {
+            return self::fileNotFound();
+        }
 
         if ($size) {
             $sizeKey = ltrim($size, '-');
-            $version = $media->versions()->where('size_key', $sizeKey)->firstOrFail();
+            $version = $media->versions()->where('size_key', $sizeKey)->first();
+            if (!$version) {
+                return self::fileNotFound($media);
+            }
             $path = MediaHelper::getStorageFolder() . '/' . $version->filename . '.' . $version->extension;
         } else {
             $path = MediaHelper::getStorageFolder() . '/' . $media->filename . '.' . $media->extension;
         }
 
         if (!Storage::disk('public')->exists($path)) {
-            abort(404, 'File not found');
+            return self::fileNotFound($media);
         }
 
-        $mimeType = $media->mime_type;
-
         return response()->file(Storage::disk('public')->path($path), [
-            'Content-Type' => $mimeType,
+            'Content-Type' => $media->mime_type,
             'Cache-Control' => 'public, max-age=31536000',
             'Content-Disposition' => 'inline; filename="' . $media->slug . '.' . $media->extension . '"',
         ]);
+    }
+
+    public function fileNotFound($media = null)
+    {
+        if ($media && in_array($media->mime_type, self::$imageMimeTypes)) {
+            return response(
+                file_get_contents(public_path('img/image-placeholder.svg')),
+                404,
+                [
+                    'Content-Type' => 'image/svg+xml',
+                    'Cache-Control' => 'public, max-age=31536000',
+                ]
+            );
+        }
+        abort(404, 'File not found.');
     }
 }
